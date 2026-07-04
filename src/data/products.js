fa348cb1606c3f4aed0be541c0d77c116ae32705
Divyanshi Shah <shahdivyanshi30@gmail.com>
2026-07-04 20:20:23 +0530

    Add files via upload

---
+++ src/data/products.js
@@ -0,0 +1,21 @@
+
+export const products = [
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    price: 99,
+    image: "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd"
+  },
+  {
+    id: 2,
+    name: "Smart Watch",
+    price: 149,
+    image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30"
+  },
+  {
+    id: 3,
+    name: "Gaming Mouse",
+    price: 59,
+    image: "https://images.unsplash.com/photo-1527814050087-3793815479db"
+  }
+];
